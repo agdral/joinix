4e278b76d2dd83d9fc5bd0507fa5f9c5b39b44d1
agdral <ezequiel.escobar@proton.me>
2026-03-23 20:24:35 -0300

    remove nixpkgs module

--- flake.nix
+++ flake.nix
@@ -1,10 +1,6 @@
 {
   description = "Joinix";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
   outputs = {...}: {
     nixosModules.default = import ./join.nix;
     homeModules.default = import ./join.nix;
